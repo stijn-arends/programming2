7e38165b6b2b45bf9074646c54a6a8a7c9625a75
stijn-arends <stijnarends@live.nl>
2022-03-26 13:15:12 +0100

    Added json parser exercise

---
+++ exercises/exercise_json_parser/json_parser.py
@@ -0,0 +1,17 @@
+# IMPORTS
+import csv
+import json
+
+from nltk.tokenize import word_tokenize
+
+
+def main():
+    """
+    Main
+    """
+    print("Test")
+    jsonFilePath = r"dSST.json"
+
+
+if __name__ == "__main__":
+    main()
